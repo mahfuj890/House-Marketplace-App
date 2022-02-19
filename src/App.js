dae5d29687c864e327b6c1d325bbd1a8d554f2bf
mahfuj890 <mahfujali490@gmail.com>
2022-02-19 13:01:32 +0600

    add user logout

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forget-password" element={<ForgetPassword />} />
         </Routes>
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,15 +1,29 @@
 import React,{useState,useEffect} from 'react';
 import { getAuth } from 'firebase/auth';
+import { useNavigate, Link } from 'react-router-dom';
+
 
 const Profile = () => {
-    const [user, setUser] = useState(null);
-    useEffect(() => {
-        const auth = getAuth();
-        setUser(auth.currentUser);
+    const auth = getAuth();
+    const [formData, setFormData] = useState({
+        name:auth.currentUser.displayName,
+        email:auth.currentUser.email
+    });
+    const navigate = useNavigate();
+    const onLogout = ()=>{
+        auth.signOut();
+        navigate("/")
+    }
 
-    }, []);
     return (
-        user ? <h1>{user.displayName}</h1> : "Not LogIn"
+        <div className='profile'>
+        <header className='profileHeader'>
+          <p className='pageHeader'>My Profile</p>
+          <button type='button' className='logOut' onClick={onLogout}>
+            Logout
+          </button>
+        </header>
+        </div>
     );
 };
 
